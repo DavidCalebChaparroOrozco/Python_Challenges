ce66e9686c43a9ad2c883f2bcd1d542487cb3280
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2022-12-23 17:40:36 -0500

    Basic information + Hint

--- Challenge01/main.py
+++ Challenge01/main.py
@@ -5,3 +5,9 @@ correo = "davidcaleb1998@hotmail.com"
 print(nombre)
 print(telefono)
 print(correo)
+
+# Hint = pista o indicación que puede apuntar un tipo de variable
+'''
+x: str = 'Hello World'
+print(x)
+'''
