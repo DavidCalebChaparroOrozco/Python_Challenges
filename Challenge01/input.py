25ab737f4f17df7e713040f768094b9822e7fdf5
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2022-12-23 18:21:07 -0500

    Input (String / Int)

---
+++ Challenge01/input.py
@@ -0,0 +1,4 @@
+# Input se utiliza para solicitar al usuario que ingrese un tipo de información en especifico
+result = input("Enter a message: ")
+print("Value entered: ",result)
+print("End of program".center(50, "*"))
