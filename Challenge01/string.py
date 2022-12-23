042484cdffc50527aaff941afdcfab2296852b2c
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2022-12-23 18:23:22 -0500

    Cadenas (String)

---
+++ Challenge01/string.py
@@ -0,0 +1,10 @@
+# String
+myFavoriteGroup = "Neffex"
+comment = "the best band"
+print("My favorite group is: ", myFavoriteGroup, comment)
+
+# int
+numA = "1"
+numB = "2"
+print("Concatenación: ", numA + numB)
+print("Suma:", int(numA) + int(numB))
