8fbf9f6366e7b04177677c2a03c57393fce3e38c
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2022-12-24 17:48:45 -0500

    Assign values to a variable

---
+++ Challenge02/Operations/operation1.py
@@ -0,0 +1,25 @@
+# Assign values to a variable
+myVar = 10
+print(myVar)
+
+# Form 1
+myVar = myVar + 1
+print(myVar)
+
+# Form 2
+myVar += 1
+print(myVar)
+
+# or maybe with subtraction for example:
+myVar = myVar - 1
+print(myVar)
+
+myVar -= 1
+print(myVar)
+
+# They can also be used for multiplication
+myVar *= 3
+print(myVar)
+
+myVar /= 2
+print(myVar)
