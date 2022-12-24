20475181a5a3f95e55744b4d32551dffe7877276
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2022-12-23 20:17:03 -0500

    Exercise01

---
+++ Challenge01/Exercise.py
@@ -0,0 +1,12 @@
+# You are requested to include the following information about a book:
+# title
+# author
+# You must print the information in the following format:
+# Provide the title: 
+# Provide the author: 
+# <title> was written by <author>.
+
+title = input('Enter the name of title of the book: ')
+author = input('Enter the name of author of the book: ')
+
+print(title, 'was written by', author)
