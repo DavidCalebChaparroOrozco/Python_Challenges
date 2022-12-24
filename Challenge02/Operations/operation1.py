32c42f5a88c504271e29d29096af0cc7455f4c71
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2022-12-24 18:09:04 -0500

    Assign values to a variable

--- Challenge02/Operations/operation1.py
+++ Challenge02/Operations/operation1.py
@@ -1,25 +1,37 @@
 # Assign values to a variable
 myVar = 10
-print(myVar)
+print(f"Variable initial value: {myVar}")
 
 # Form 1
 myVar = myVar + 1
-print(myVar)
+print(f"First form for by add value: {myVar}")
 
 # Form 2
 myVar += 1
-print(myVar)
+print(f"Second form for by add value: {myVar}")
 
 # or maybe with subtraction for example:
 myVar = myVar - 1
-print(myVar)
+print(f"First form for by subtraction value: {myVar}")
 
 myVar -= 1
-print(myVar)
+print(f"Second form for by subtraction value: {myVar}")
 
 # They can also be used for multiplication
 myVar *= 3
-print(myVar)
+print(f"Form for multiplying values: {myVar}")
 
+# Division
 myVar /= 2
-print(myVar)
+print(f"Form for divide values: {myVar}")
+
+# Exponentiation
+print("Here I overwrote the variable to give clarity with the operation")
+myVar = 12
+print(f" The new value is: {myVar} ".center(50,"-")) 
+myVar **=2
+print(f"Form to exponentiation values: {myVar}")
+
+# Integer Division
+myVar //= 12
+print(f"Form to integer square root: {myVar}")
