fd8dde773ff8f677b7a5b6bfbcc4ed9947a0e0af
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2022-12-24 18:22:27 -0500

    renamed:    Challenge01/Exercise.py -> Challenge01/Exercise01.py renamed:    Challenge02/Exercise.py -> Challenge02/Operations/Exercise02.py modified:   Challenge02/Operations/operation1.py new file:   Challenge02/Operations/operation2.py

--- Challenge02/Operations/operation1.py
+++ Challenge02/Operations/operation1.py
@@ -4,34 +4,34 @@ print(f"Variable initial value: {myVar}")
 
 # Form 1
 myVar = myVar + 1
-print(f"First form for by add value: {myVar}")
+print(f"First form for by + : {myVar}")
 
 # Form 2
 myVar += 1
-print(f"Second form for by add value: {myVar}")
+print(f"Second form for by += : {myVar}")
 
 # or maybe with subtraction for example:
 myVar = myVar - 1
-print(f"First form for by subtraction value: {myVar}")
+print(f"First form for by - : {myVar}")
 
 myVar -= 1
-print(f"Second form for by subtraction value: {myVar}")
+print(f"Second form for by -= : {myVar}")
 
 # They can also be used for multiplication
 myVar *= 3
-print(f"Form for multiplying values: {myVar}")
+print(f"Form for *= : {myVar}")
 
 # Division
 myVar /= 2
-print(f"Form for divide values: {myVar}")
+print(f"Form for /= : {myVar}")
 
 # Exponentiation
 print("Here I overwrote the variable to give clarity with the operation")
 myVar = 12
 print(f" The new value is: {myVar} ".center(50,"-")) 
 myVar **=2
-print(f"Form to exponentiation values: {myVar}")
+print(f"Form to **= : {myVar}")
 
 # Integer Division
 myVar //= 12
-print(f"Form to integer square root: {myVar}")
+print(f"Form to //= : {myVar}")
---
+++ Challenge02/Operations/operation2.py
@@ -0,0 +1,9 @@
+# Comparison operations
+numA = 2
+numB = 4
+
+result = numA == numB
+print(f"The result == : {result}")
+
+result = numA != numB
+print(f"The result != : {result}")
