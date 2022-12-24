cd3fea991a86ce528ec5f35ea00a71d75193c79c
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2022-12-23 19:53:17 -0500

    input(Int)

---
+++ Challenge01/inputInt.py
@@ -0,0 +1,5 @@
+# Solo recibe los valores numericos no puede recibir datos categoricos por la función int
+numA = int(input("Enter the first number: "))
+numB = int(input("Enter the second number: "))
+result = numA + numB
+print("The result of the sum is: ", result)
