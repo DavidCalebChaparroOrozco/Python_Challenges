66b67c1c064144220d650cda419be84f78f56e9d
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-04 16:19:39 -0500

    Operator NOT

---
+++ Challenge02/Operations/Exercise07.py
@@ -0,0 +1,13 @@
+# Operator NOT
+# Ask a parent if he/she can attend one of his/her child's games.
+# You have the following cases:
+# If you have a vacation or it is a day off, then yes you can attend the son's game. 
+# Otherwise he cannot attend, as he would be busy.
+
+vacation = True
+dayOff = False
+
+if not(vacation or dayOff):
+    print('Cannot attend the game')
+else:
+    print('You can attend the game')
