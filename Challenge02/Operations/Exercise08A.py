06169c65a7d323cb96394e870175a6a24cc288b9
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-04 17:08:41 -0500

    Inside the range (20's) or (30's)

---
+++ Challenge02/Operations/Exercise08A.py
@@ -0,0 +1,18 @@
+age = int(input('Enter your age: '))
+
+twenties = (age >= 20 and age < 30)
+print(twenties)
+
+thirties = (age >= 30 and age < 40)
+print(thirties)
+
+if twenties or thirties:
+    #print('Inside the range (20\'s) or (30\'s)')
+    if twenties:
+        print('within Twenty')
+    elif thirties:
+        print('within Thirty')
+    else:
+        print('Out of range')
+else:
+    print("Is not among 20's or 30's")
