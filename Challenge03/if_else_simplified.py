f1a75625183cc529532ca32f2dc0247c6deeabc7
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-12 23:28:31 -0500

    if_else_simplified (Ternary operator)

---
+++ Challenge03/if_else_simplified.py
@@ -0,0 +1,6 @@
+condition = True
+
+print('Condition True') if condition else print('Condition False')
+
+# Note: The 'elif' cannot be used in ternary operations, it is only advisable to use them when you have short operations
+# such as the one in this exercise.
