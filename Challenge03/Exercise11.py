170210b9f268cbadb07ba7315d7f0c992eac1165
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-12 22:51:46 -0500

    Number to text conversion

---
+++ Challenge03/Exercise11.py
@@ -0,0 +1,14 @@
+# Number to text conversion
+numA = int(input('Enter a value between 1 and 3: '))
+numText = ''
+
+if numA == 1:
+    numText= 'Number One'
+elif numA ==2:
+    numText = 'Number Two'
+elif numA ==3:
+    numText = 'Num Three'
+else:
+    numText = 'Out of range value'
+
+print(f'Number provided: {numA}: {numText}')
