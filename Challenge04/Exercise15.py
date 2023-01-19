90cd619b215cf7726de59f6bcf4f19d73f4ce558
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-19 17:15:19 -0500

    Print integers from 0 to 10 using the while loop

---
+++ Challenge04/Exercise15.py
@@ -0,0 +1,8 @@
+# Print integers from 0 to 10 using the while loop
+count = 0
+max = 10
+while count <= max:
+    print(count)
+    count += 1 # count = count + 1
+else:
+    print('End while loop')
