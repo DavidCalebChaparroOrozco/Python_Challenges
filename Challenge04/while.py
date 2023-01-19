4fd272b0f21ad76faea058035ed8f9777386c00a
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-19 16:53:23 -0500

    modified: README.md

---
+++ Challenge04/while.py
@@ -0,0 +1,13 @@
+condition = True
+
+# while condition:
+#     print('Running while loop')
+# else:
+#     print('End while loop')
+
+count = 0
+while count < 3:
+    print(count)
+    count += 1 # count = count + 1
+else:
+    print('End while loop')
