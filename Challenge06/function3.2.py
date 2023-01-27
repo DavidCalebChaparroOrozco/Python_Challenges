668da49e9e3ab6da454c902a1f55ae8e099ff695
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-26 23:20:42 -0500

    Function (Default values) + hint

---
+++ Challenge06/function3.2.py
@@ -0,0 +1,9 @@
+# if we want to leave a note or hint
+def sum(numA= 15, numB=20) -> int:
+    return numA + numB
+
+result = sum()
+print(f'Result sum: {result}')
+
+print(f'Result sum: {sum(2,15)}')
+
