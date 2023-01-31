b3037c3f8392665e2662a92c41265d956749826d
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-30 23:28:19 -0500

    Exercise: Create a function to multiply the received values of numeric type

--- Challenge06/Exercise20.py
+++ Challenge06/Exercise20.py
@@ -2,11 +2,12 @@
 # using variable arguments *args as the function parameter
 # and return as result the multiplication of all values passed as arguments.
 
-def multiply_values(*numbers) ->int:
+def multiply_values(*numbers) -> int:
     result = 1
     for number in numbers:
         # result = result * value
         result *= number
     return result
 
-print(multiply_values(3,6,9,2,3,1,10))
+
+print(multiply_values(3, 6, 9, 2, 3, 1, 10))
