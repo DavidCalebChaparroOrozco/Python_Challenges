ce2727072e14e1273dbfc31fbd6dd2485167a459
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-30 22:32:23 -0500

    different types of data as arguments

---
+++ Challenge06/function06.py
@@ -0,0 +1,12 @@
+# display_names
+def displayNames(names):
+    for name in names:
+        print(name)
+
+names = ['Rick', 'Morty', 'Summer']
+displayNames(names)
+
+displayNames('Jerry')
+
+displayNames((1,2))
+displayNames([10,20])
