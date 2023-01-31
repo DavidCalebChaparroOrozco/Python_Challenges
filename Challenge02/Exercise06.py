80452e459a05b71f47ee1376845c185eb069a406
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-30 23:15:35 -0500

    Exercise: Operator OR

--- Challenge02/Exercise06.py
+++ Challenge02/Exercise06.py
@@ -1,8 +1,8 @@
 # Operator OR
 # Ask a parent if he/she can attend one of his/her child's games.
 # You have the following cases:
-# If you have a vacation or it is a day off, then yes you can attend the son's game. 
-# Otherwise he cannot attend, as he would be busy.
+# If you have a vacation, or it is a day off, then yes you can attend the son's game.
+# Otherwise, he cannot attend, as he would be busy.
 
 vacation = True
 dayOff = False
