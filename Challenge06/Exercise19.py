69b594ea3bb48b1d2a8fe1c983c553e3f39cc0b4
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-30 21:45:05 -0500

    Create a function to sum the received values of numeric type

---
+++ Challenge06/Exercise19.py
@@ -0,0 +1,12 @@
+# Create a function to sum the received values of numeric type,
+# using variable arguments *args as the function parameter
+# and return as result the sum of all values passed as arguments.
+
+def sum_values(*args) ->int:
+    result = 0
+    for value in args:
+        # result = result + value
+        result += value
+    return result
+
+print(sum_values(3,6,9,2,3,1,10))
