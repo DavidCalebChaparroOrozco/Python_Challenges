876ebf890e200a9c1772d52e8bc020f46b9680a5
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-30 23:18:26 -0500

    Exercise: Temperature Converter

--- Challenge06/Exercise23.py
+++ Challenge06/Exercise23.py
@@ -1,10 +1,12 @@
-# Perform two functions to convert from degrees Celsius to fahrenheit and vice versa.
+# Perform two functions to convert from degrees Celsius to Fahrenheit and vice versa.
 
 def celsius_fahrenheit(celsius):
-    return celsius * 9/5 + 32
+    return celsius * 9 / 5 + 32
+
 
 def fahrenheit_celsius(fahrenheit):
-    return (fahrenheit - 32) * 5/9
+    return (fahrenheit - 32) * 5 / 9
+
 
 celsius = float(input('Enter value in Celsius: '))
 result = celsius_fahrenheit(celsius)
