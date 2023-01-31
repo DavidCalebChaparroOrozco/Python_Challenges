7fc30d5604ecf4bd1fd205b9dae22bfdeb67d6f7
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-01-30 23:28:49 -0500

    Exercise: Print numbers in descending order using recursive functions.

--- Challenge06/Exercise21.py
+++ Challenge06/Exercise21.py
@@ -2,13 +2,14 @@
 # Can be any positive value, if negative values are passed, nothing is printed.
 
 def print_number_recursive(number):
-    if number >=1:
+    if number >= 1:
         print(number)
         print_number_recursive(number - 1)
     elif number == 0:
         return
-    elif number <=0:
+    elif number <= 0:
         print('Wrong value...')
+
 
 numberA = int(input('Enter the number: '))
 print_number_recursive(numberA)
