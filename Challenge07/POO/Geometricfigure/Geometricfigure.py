fe865996bef50919d04ca708274376fb89c47d31
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-02-01 18:49:16 -0500

    Multiple Inheritance / (MRO) / Validations

--- Challenge07/POO/Geometricfigure/Geometricfigure.py
+++ Challenge07/POO/Geometricfigure/Geometricfigure.py
@@ -1,21 +1,40 @@
 class GeometricFigure:
     def __init__(self, width, height):
-        self._width = width
-        self._height = height
+        if self._valide_value(width):
+            self._width = width
+        else:
+            self._width = 0
+            print(f'Wrong width value {width}')
+        if self._valide_value(height):
+            self._height = height
+        else:
+            self._height = 0
+            print(f'Wrong width value {height}')
 
     @property
     def width(self):
         return self._width
 
+    @width.setter
+    def width(self, width):
+        if self._valide_value(width):
+            self._width = width
+        else:
+            print(f'Wrong width value: {width}')
+
     @property
     def height(self):
         return self._height
 
-    @width.setter
-    def first_name(self, width):
-        self._width = width
-
     @height.setter
-    def age(self, height):
-        self._height = height
+    def height(self, height):
+        if self._valide_value(height):
+            self._height = height
+        else:
+            print(f'Wrong height value {height}')
 
+    def __str__(self):
+        return f'Geometric figure [Width: {self._width}, Height: {self._height}]'
+
+    def _valide_value(self, value):
+        return True if 0 < value < 10 else False
