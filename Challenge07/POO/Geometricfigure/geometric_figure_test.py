06ed68d67c73659ebe79c482e6f62fb3dc89c9ca
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-02-01 18:43:36 -0500

    Multiple Inheritance / (MRO) / Validations

--- Challenge07/POO/Geometricfigure/geometric_figure_test.py
+++ Challenge07/POO/Geometricfigure/geometric_figure_test.py
@@ -1,11 +1,24 @@
 from Square import *
+from Rectangle import *
 
-square1 = Square(5,'Red')
+
+print('Creation of square object'.center(50,'-'))
+square1 = Square(side=5,color='Red')
+square1.side = -10
 # print(square1.width)
 # print(square1.height)
 # print(square1.color)
 print(f'Calculate square area: {square1.calculate_area()}')
+print(square1)
+
 
 # MRO - Method Resolution Order
 # Is the order in which base classes are searched for a member during lookup.
-print(Square.mro())
+# print(Square.mro())
+
+
+print('Creation of rectangle object'.center(50,'-'))
+rectangle1 = Rectangle(width=9,height=8,color='Green')
+rectangle1.height = 8
+print(f'Calculate rectangle area: {rectangle1.calculate_area()}')
+print(rectangle1)
